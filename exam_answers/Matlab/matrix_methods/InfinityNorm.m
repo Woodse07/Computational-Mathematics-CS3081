95d76ab67411cf4056f0daf1be4bc106f2d5a1da
Woodse07 <sewoods@tcd.ie>
2019-04-21 15:56:10 +0100

    Added infinitNorm.m

---
+++ exam_answers/Matlab/matrix_methods/InfinityNorm.m
@@ -0,0 +1,25 @@
+function N = InfinityNorm(A)
+% Getting dimensions of matrix..
+[m, n] = size(A);    
+% If matrix is not square, output error message and return..
+if (m ~= n)
+    disp("The matrix must be square");
+    return;
+end
+% Setting up some variables
+max = 0;
+temp = 0;
+
+% Get sum of all values of all rows and keep the max..
+for i = 1:m
+    for j = 1:n
+        temp = temp + abs(A(i,j));
+    end
+    if (temp > max)
+        max = temp;
+    end
+    temp = 0;
+end
+disp(max);
+N = max;
+end
